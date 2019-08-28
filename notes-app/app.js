5bb284e57cdc858d6051cb22785fbdf9f835c892
David Wightman <david@davidwightman.org>
2019-08-27 21:58:31 -0400

    added yargs package and worked on notes app

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,58 @@
 // import validator from 'validator';
 // import { getNotes } from 'notes';
-const getNotes = require('./notes.js')
+const getNotes = require('./notes.js');
+const chalk = require('chalk');
+const yargs = require('yargs');
 
-var validator = require('validator');
+//const command = process.argv[2]
 
-const notes = getNotes();
-console.log(notes)
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+    }
+})
 
-console.log(validator.isURL('https://www.davi.com'))
+// create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    handler: function () {
+        console.log('removing the note')
+    }
+})
+
+// create list command
+yargs.command({
+    command: 'list',
+    describe: 'listing out all of the notes',
+    handler: function () {
+        console.log('listing out all of the notes')
+    }
+})
+
+// create list command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('reading a note')
+    }
+})
+
+console.log(process.argv)
+yargs.parse()
+// if (command === 'add') {
+//     console.log('adding note!')
+// } else if (command === 'remove') {
+//     console.log('removing note')
+// }
