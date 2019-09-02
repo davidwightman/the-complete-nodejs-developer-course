d8a256ed157a55b7665b371c5708480b2ce551ff
David Wightman <david@davidwightman.org>
2019-09-02 08:42:41 -0400

    object destructuring in weather app, broke up callbacks

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,24 @@
 require('dotenv').config()
-const request = require('request');
+const geocode = require('./utils/geocode')
+const forcast = require('./utils/forcast')
 
-const url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/37.8267,-122.4233?units=us`
+const inputFromUser = process.argv[2]
 
-const geoCode = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAP_BOX_ACCESS_TOKEN}&limit=1`
-
-request({url: geoCode, json: true}, (error, response) => {
-    //console.log(response.features)
-    console.log(response.body.features[0].place_name)
-    console.log(response.body.features[0].center[0]) // longitude
-    console.log(response.body.features[0].center[1]) // latitude
-})
-// features[0].place_name
-// longitude features[0].center[0]
-// latitude features[0].center[1]
-
-// request({url: url, json: true}, (error, response)=>{
-//   // console.log(response.body.currently)
-//   console.log(response.body.daily.data[0].summary)
-//   console.log(`it is currently ${response.body.currently.temperature} there is ${response.body.currently.precipProbability}% chance of rain`)
-// });
+if (!inputFromUser) {
+    console.log('Try again but include a city')
+} else {
+    geocode(inputFromUser, (error, { longitude, latitude, location }) => {
+        if (error) {
+            return console.log(error)
+        }
+        forcast(longitude, latitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            
+            console.log(location)
+            console.log(forecastData)
+    
+        })
+    })
+}
